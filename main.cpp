2bf4823b4668309da8ba4929f11071c3701e27bf
DawKaz111 <dawidkazmierczak35@gmail.com>
2026-04-11 11:21:46 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,101 +1,261 @@
 #include <iostream>
 #include <chrono>
-#include <cstdlib>
 #include <iomanip>
+#include <string>
+#include <random>
+
 #include "ArrayList.h"
+#include "SinglyLinkedList.h"
+#include "DoublyLinkedList.h"
 
 using namespace std;
 using namespace std::chrono;
 
-// Zwykłe tablice zamiast std::vector
-const unsigned int SEEDS[] = {123, 456, 789, 1011, 1314, 1617, 1920, 2223, 2526, 2829};
-const int SIZES[] = {5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000};
+const unsigned int seedy[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+const int rozmiary[] = {5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000};
 
-int bigRand() {
-    return (static_cast<int>(rand()) << 15) | rand();
+void testArrayList() {
+    cout << "\nTABLICA DYNAMICZNA:\n";
+
+    for (int i = 0; i < 8; ++i) {
+        int rozmiar = rozmiary[i];
+        long long dodPocz = 0, dodKon = 0, dodNa = 0;
+        long long usunPocz = 0, usunKon = 0, usunNa = 0, wyszukiwanie = 0;
+
+        for (int j = 0; j < 10; ++j) {
+            mt19937 gen(seedy[j]);
+            uniform_int_distribution<int> zakres_wartosci(0, 1000000000);
+            uniform_int_distribution<int> indeksy(0, rozmiar - 1);
+
+            int* danePoczatkowe = new int[rozmiar];
+            for (int k = 0; k < rozmiar; ++k) danePoczatkowe[k] = zakres_wartosci(gen);
+
+            int wartosc = zakres_wartosci(gen);
+            int losowy_index = indeksy(gen);
+            int szukana_wartosc = zakres_wartosci(gen);
+
+            ArrayList kopie[10];
+            for (int k = 0; k < 10; ++k) {
+                for (int m = 0; m < rozmiar; ++m) {
+                    kopie[k].dodKon(danePoczatkowe[m]);
+                }
+            }
+            delete[] danePoczatkowe;
+
+            auto start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].dodPocz(wartosc);
+            auto koniec = high_resolution_clock::now();
+            dodPocz += duration_cast<nanoseconds>(koniec - start).count() / 10;
+            for (int k = 0; k < 10; ++k) kopie[k].usunPocz();
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].dodKon(wartosc);
+            koniec = high_resolution_clock::now();
+            dodKon += (duration_cast<nanoseconds>(koniec - start).count() / 10);
+            for (int k = 0; k < 10; ++k) kopie[k].usunKon();
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].dodNa(losowy_index, wartosc);
+            koniec = high_resolution_clock::now();
+            dodNa += duration_cast<nanoseconds>(koniec - start).count() / 10;
+            for (int k = 0; k < 10; ++k) kopie[k].usunNa(losowy_index);
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].wyszukaj(szukana_wartosc);
+            koniec = high_resolution_clock::now();
+            wyszukiwanie += duration_cast<nanoseconds>(koniec - start).count() / 10;
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].usunPocz();
+            koniec = high_resolution_clock::now();
+            usunPocz += duration_cast<nanoseconds>(koniec - start).count() / 10;
+            for (int k = 0; k < 10; ++k) kopie[k].dodPocz(wartosc);
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].usunKon();
+            koniec = high_resolution_clock::now();
+            usunKon += duration_cast<nanoseconds>(koniec - start).count() / 10;
+            for (int k = 0; k < 10; ++k) kopie[k].dodKon(wartosc);
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].usunNa(losowy_index);
+            koniec = high_resolution_clock::now();
+            usunNa += duration_cast<nanoseconds>(koniec - start).count() / 10;
+        }
+
+        cout << "\nSTATYSTKI DLA N = " << rozmiar << endl;
+        cout << "\nDODAWANIA:\n";
+        cout << "Dodawanie na poczatku: " << dodPocz / 10 << "\nDodawanie na koncu: " << dodKon / 10 << "\nDodawanie w danym miejscu: " << dodNa / 10 << endl;
+        cout << "\nODEJMOWANIA:\n";
+        cout << "Usuwanie na poczatku: " << usunPocz / 10 << "\nUsuwanie na koncu: " << usunKon / 10 << "\nUsuwanie w danym miejscu: " << usunNa / 10 << endl;
+        cout << "\nWYSZUKIWANIE: " << wyszukiwanie / 10 << endl;
+    }
+    cout << "\n--------------------------------\n";
+}
+
+void testSinglyLinkedList() {
+    cout << "\nLISTA JEDNOKIERUNKOWA:\n";
+
+    for (int i = 0; i < 8; ++i) {
+        int rozmiar = rozmiary[i];
+        long long dodPocz = 0, dodKon = 0, dodNa = 0;
+        long long usunPocz = 0, usunKon = 0, usunNa = 0, wyszukiwanie = 0;
+
+        for (int j = 0; j < 10; ++j) {
+            mt19937 gen(seedy[j]);
+            uniform_int_distribution<int> zakres_wartosci(0, 1000000000);
+            uniform_int_distribution<int> indeksy(0, rozmiar - 1);
+
+            int* danePoczatkowe = new int[rozmiar];
+            for (int k = 0; k < rozmiar; ++k) danePoczatkowe[k] = zakres_wartosci(gen);
+
+            int wartosc = zakres_wartosci(gen);
+            int losowy_index = indeksy(gen);
+            int szukana_wartosc = zakres_wartosci(gen);
+
+            SinglyLinkedList kopie[10];
+            for (int k = 0; k < 10; ++k) {
+                for (int m = 0; m < rozmiar; ++m) {
+                    kopie[k].addAtEnd(danePoczatkowe[m]);
+                }
+            }
+            delete[] danePoczatkowe;
+
+            auto start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].addAtBeginning(wartosc);
+            auto koniec = high_resolution_clock::now();
+            dodPocz += duration_cast<nanoseconds>(koniec - start).count() / 10;
+            for (int k = 0; k < 10; ++k) kopie[k].removeAtBeginning();
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].addAtEnd(wartosc);
+            koniec = high_resolution_clock::now();
+            dodKon += duration_cast<nanoseconds>(koniec - start).count() / 10;
+            for (int k = 0; k < 10; ++k) kopie[k].removeAtEnd();
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].addAtPos(losowy_index, wartosc);
+            koniec = high_resolution_clock::now();
+            dodNa += duration_cast<nanoseconds>(koniec - start).count() / 10;
+            for (int k = 0; k < 10; ++k) kopie[k].removeAtPos(losowy_index);
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].find(szukana_wartosc);
+            koniec = high_resolution_clock::now();
+            wyszukiwanie += duration_cast<nanoseconds>(koniec - start).count() / 10;
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].removeAtBeginning();
+            koniec = high_resolution_clock::now();
+            usunPocz += duration_cast<nanoseconds>(koniec - start).count() / 10;
+            for (int k = 0; k < 10; ++k) kopie[k].addAtBeginning(wartosc);
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].removeAtEnd();
+            koniec = high_resolution_clock::now();
+            usunKon += duration_cast<nanoseconds>(koniec - start).count() / 10;
+            for (int k = 0; k < 10; ++k) kopie[k].addAtEnd(wartosc);
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].removeAtPos(losowy_index);
+            koniec = high_resolution_clock::now();
+            usunNa += duration_cast<nanoseconds>(koniec - start).count() / 10;
+        }
+
+        cout << "\nSTATYSTKI DLA N = " << rozmiar << endl;
+        cout << "\nDODAWANIA:\n";
+        cout << "Dodawanie na poczatku: " << dodPocz / 10 << "\nDodawanie na koncu: " << dodKon / 10 << "\nDodawanie w danym miejscu: " << dodNa / 10 << endl;
+        cout << "\nODEJMOWANIA:\n";
+        cout << "Usuwanie na poczatku: " << usunPocz / 10 << "\nUsuwanie na koncu: " << usunKon / 10 << "\nUsuwanie w danym miejscu: " << usunNa / 10 << endl;
+        cout << "\nWYSZUKIWANIE: " << wyszukiwanie / 10 << endl;
+    }
+    cout << "\n--------------------------------\n";
+}
+
+void testDoublyLinkedList() {
+    cout << "\nLISTA DWUKIERUNKOWA:\n";
+
+    for (int i = 0; i < 8; ++i) {
+        int rozmiar = rozmiary[i];
+        long long dodPocz = 0, dodKon = 0, dodNa = 0;
+        long long usunPocz = 0, usunKon = 0, usunNa = 0, wyszukiwanie = 0;
+
+        for (int j = 0; j < 10; ++j) {
+            mt19937 gen(seedy[j]);
+            uniform_int_distribution<int> zakres_wartosci(0, 1000000000);
+            uniform_int_distribution<int> indeksy(0, rozmiar - 1);
+
+            int* danePoczatkowe = new int[rozmiar];
+            for (int k = 0; k < rozmiar; ++k) danePoczatkowe[k] = zakres_wartosci(gen);
+
+            int wartosc = zakres_wartosci(gen);
+            int losowy_index = indeksy(gen);
+            int szukana_wartosc = zakres_wartosci(gen);
+
+            DoublyLinkedList kopie[10];
+            for (int k = 0; k < 10; ++k) {
+                for (int m = 0; m < rozmiar; ++m) {
+                    kopie[k].addAtEnd(danePoczatkowe[m]);
+                }
+            }
+            delete[] danePoczatkowe;
+
+            auto start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].addAtBeginning(wartosc);
+            auto koniec = high_resolution_clock::now();
+            dodPocz += duration_cast<nanoseconds>(koniec - start).count() / 10;
+            for (int k = 0; k < 10; ++k) kopie[k].removeAtBeginning();
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].addAtEnd(wartosc);
+            koniec = high_resolution_clock::now();
+            dodKon += duration_cast<nanoseconds>(koniec - start).count() / 10;
+            for (int k = 0; k < 10; ++k) kopie[k].removeAtEnd();
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].addAtPos(losowy_index, wartosc);
+            koniec = high_resolution_clock::now();
+            dodNa += duration_cast<nanoseconds>(koniec - start).count() / 10;
+            for (int k = 0; k < 10; ++k) kopie[k].removeAtPos(losowy_index);
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].find(szukana_wartosc);
+            koniec = high_resolution_clock::now();
+            wyszukiwanie += duration_cast<nanoseconds>(koniec - start).count() / 10;
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].removeAtBeginning();
+            koniec = high_resolution_clock::now();
+            usunPocz += duration_cast<nanoseconds>(koniec - start).count() / 10;
+            for (int k = 0; k < 10; ++k) kopie[k].addAtBeginning(wartosc);
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].removeAtEnd();
+            koniec = high_resolution_clock::now();
+            usunKon += duration_cast<nanoseconds>(koniec - start).count() / 10;
+            for (int k = 0; k < 10; ++k) kopie[k].addAtEnd(wartosc);
+
+            start = high_resolution_clock::now();
+            for (int k = 0; k < 10; ++k) kopie[k].removeAtPos(losowy_index);
+            koniec = high_resolution_clock::now();
+            usunNa += duration_cast<nanoseconds>(koniec - start).count() / 10;
+
+        }
+
+        cout << "\nSTATYSTKI DLA N = " << rozmiar << endl;
+        cout << "\nDODAWANIA:\n";
+        cout << "Dodawanie na poczatku: " << dodPocz / 10 << "\nDodawanie na koncu: " << dodKon / 10 << "\nDodawanie w danym miejscu: " << dodNa / 10 << endl;
+        cout << "\nODEJMOWANIA:\n";
+        cout << "Usuwanie na poczatku: " << usunPocz / 10 << "\nUsuwanie na koncu: " << usunKon / 10 << "\nUsuwanie w danym miejscu: " << usunNa / 10 << endl;
+        cout << "\nWYSZUKIWANIE: " << wyszukiwanie / 10 << endl;
+    }
+    cout << "\n--------------------------------\n";
 }
 
 int main() {
-    cout << "Pomiary (srednia z 10 seedow) w nanosekundach:\n";
-    cout << "--------------------------------------------------------------------------------------------------\n";
-    cout << setw(8) << "Rozmiar" << " | " 
-         << setw(10) << "AddFront" << " | " 
-         << setw(10) << "AddBack" << " | " 
-         << setw(10) << "AddRand" << " | " 
-         << setw(10) << "RemFront" << " | " 
-         << setw(10) << "RemBack" << " | " 
-         << setw(10) << "RemRand" << " | " 
-         << setw(10) << "Search" << endl;
-    cout << "--------------------------------------------------------------------------------------------------\n";
-
-    for (int i = 0; i < 8; ++i) { // Dla każdego z 8 rozmiarów
-        int currentSize = SIZES[i];
-        
-        long long sumAddF = 0, sumAddB = 0, sumAddR = 0;
-        long long sumRemF = 0, sumRemB = 0, sumRemR = 0;
-        long long sumSearch = 0;
-
-        for (int j = 0; j < 10; ++j) { // Dla każdego z 10 seedów
-            srand(SEEDS[j]);
-            ArrayList arr(currentSize);
-
-            // 1. Wypełnianie tablicy do zadanego rozmiaru
-            for (int k = 0; k < currentSize; ++k) {
-                arr.dodKon(rand());
-            }
-
-            // Losujemy dane do testów
-            int val = rand();
-            int randIdx = bigRand() % currentSize;
-
-            // --- POMIARY DODAWANIA ---
-            auto start = high_resolution_clock::now();
-            arr.dodPocz(val);
-            sumAddF += duration_cast<nanoseconds>(high_resolution_clock::now() - start).count();
-            arr.usunPocz(); // Przywrócenie rozmiaru
-
-            start = high_resolution_clock::now();
-            arr.dodKon(val);
-            sumAddB += duration_cast<nanoseconds>(high_resolution_clock::now() - start).count();
-            arr.usunKon();
-
-            start = high_resolution_clock::now();
-            arr.dodNa(randIdx, val);
-            sumAddR += duration_cast<nanoseconds>(high_resolution_clock::now() - start).count();
-            arr.usunNa(randIdx);
-
-            // --- POMIAR WYSZUKIWANIA ---
-            int searchVal = rand();
-            start = high_resolution_clock::now();
-            arr.wyszukaj(searchVal);
-            sumSearch += duration_cast<nanoseconds>(high_resolution_clock::now() - start).count();
-
-            // --- POMIARY USUWANIA ---
-            start = high_resolution_clock::now();
-            arr.usunPocz();
-            sumRemF += duration_cast<nanoseconds>(high_resolution_clock::now() - start).count();
-            arr.dodPocz(val);
-
-            start = high_resolution_clock::now();
-            arr.usunKon();
-            sumRemB += duration_cast<nanoseconds>(high_resolution_clock::now() - start).count();
-            arr.dodKon(val);
-
-            start = high_resolution_clock::now();
-            arr.usunNa(randIdx);
-            sumRemR += duration_cast<nanoseconds>(high_resolution_clock::now() - start).count();
-        }
-
-        // Wypisanie średniej (dzielone przez 10)
-        cout << setw(8) << currentSize << " | "
-             << setw(10) << sumAddF / 10 << " | "
-             << setw(10) << sumAddB / 10 << " | "
-             << setw(10) << sumAddR / 10 << " | "
-             << setw(10) << sumRemF / 10 << " | "
-             << setw(10) << sumRemB / 10 << " | "
-             << setw(10) << sumRemR / 10 << " | "
-             << setw(10) << sumSearch / 10 << endl;
-    }
+    testArrayList();
+    testSinglyLinkedList();
+    testDoublyLinkedList();
 
     return 0;
 }
